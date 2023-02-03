d68ddcba56d6b6c74dd875274de5f0a8339d0053
Batuhan Akçan <batuhanakcan38@gmail.com>
2023-02-03 15:07:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = "batuhan"
+
+@app.route("/home")
+@app.route("/")
+def home():
+    if "user" in session:
+        user = session["user"]
+        return render_template("home.html", usr=user)
+    else:
+        return render_template("home.html", usr=None)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        session["user"] = user
+        session.permanent = True
+        return redirect(url_for("home"))
+    elif "user" in session:
+        user = session["user"]
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("home"))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
