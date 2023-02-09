b38d95547074223502dc6b8e9f8be64516483fb1
Batuhan Akçan <batuhanakcan38@gmail.com>
2023-02-09 12:04:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "batuhan"
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route("/home")
 @app.route("/")
